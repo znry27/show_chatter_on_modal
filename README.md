7a7974eea1698798d9b36af2b66bb83eebeada8d
znry27 <z.nry27@gmail.com>
2019-06-21 00:39:27 +0700

    First Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Odoo Show Chatter on Modal
+
+By default Odoo will hide Chatter when form is open in modal. For example when click one2many fields.
+
+This module override that function. Show you can see Chatter in modal like normal form.
---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Show Chatter on Modal',
+    'version' : '1.0.0',
+    'author' : 'Ngasturi',
+    'summary': 'Show Chatter on Modal',
+    'description': 'Show Chatter on Modal',
+    'category': '',
+    'website': 'http://cahngloji.blogspot.com/',    
+    'depends' : ['web'],
+    'data': [
+        'views/view.xml',
+    ],
+    'demo': [
+    ],
+    'qweb': [
+
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
